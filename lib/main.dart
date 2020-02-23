3885549ab3755b0d1edc184e9b2592a7c5f0b0c9
BangHyeonJun <bhj2849@naver.com>
2020-02-23 16:11:18 +0900

    [ENH] 프로젝트 골격 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,15 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text("Hello World"),
-        ),
-      ),
-    );
+        title: 'Flutter layout demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Flutter layout demo"),
+            ),
+            body: Center(child: Text("Hello Wolrd!"))));
   }
 }
